dee144bed130536f4c841aa05bf5e8d3f53a3679
max-tantan <fatanjamterbang@gmail.com>
2026-08-05 09:58:32 +0700

    ubah doang url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'https://jhicbe-8xf0qpql.b4a.run/',
+        target: 'https://jhicbe-fuhw7ej0.b4a.run/',
         changeOrigin: true,
         rewrite: (path) => path,
       },
